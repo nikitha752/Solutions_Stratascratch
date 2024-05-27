54d5787eb717cc1ae0d621d5790042e51be4404b
Nikitha <75156918+nikitha752@users.noreply.github.com>
2024-05-27 22:45:17 +0530

    Update Q13.py

--- Q13.py
+++ Q13.py
@@ -1,1 +1,11 @@
+#Find the genre of the person with the most number of oscar winnings
 
+import pandas as pd
+
+df=oscar_nominees[oscar_nominees['winner']]
+df=df.groupby('nominee').size().to_frame('count').reset_index()
+
+res = pd.merge(df,nominee_information, left_on='nominee', right_on='name', how='left')
+
+
+res.groupby(['nominee','top_genre']).size().to_frame('count').reset_index().sort_values(['count','nominee'], ascending= [False,True]).head(1)[['top_genre']]
