fb3e0c06f535900dc2928c7e67e1dab7c5f9d7aa
Nikitha <75156918+nikitha752@users.noreply.github.com>
2024-05-30 00:58:04 +0530

    Update Q17.py

--- Q17.py
+++ Q17.py
@@ -1,2 +1,28 @@
 #Find the variance and the standard deviation of scores that have grade A
 
+import pandas as pd
+import numpy as np
+
+df =los_angeles_restaurant_health_inspections[los_angeles_restaurant_health_inspections['grade']=='A']
+df['sub']= 0
+df['mean']= np.mean(df['score'])
+
+def fun(row):
+    row['sub']= (row['score']-row['mean'])**2
+    return row
+df = df.apply(fun, axis=1)
+variance= np.mean(df['sub'])
+std= np.sqrt(variance)
+pd.DataFrame({'variance': variance, 'std': std}, index=[0])
+
+#or
+
+import pandas as pd
+import numpy as np
+
+df =los_angeles_restaurant_health_inspections[los_angeles_restaurant_health_inspections['grade']=='A']
+main = df[['score']]
+main['mean']= np.mean(df['score'])
+main['x']= (main['score']-main['mean'])**2
+main['variance']= np.mean(main['x'])
+main['std']= np.sqrt(main['variance'])
