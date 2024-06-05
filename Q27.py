00d945f1eb1b4677ba96498625d7fe5ce386c2fd
Nikitha <75156918+nikitha752@users.noreply.github.com>
2024-06-06 02:34:08 +0530

    Update Q27.py

--- Q27.py
+++ Q27.py
@@ -1,1 +1,4 @@
+import pandas as pd
 
+crunchbase_acquisitions['quarter'] = crunchbase_acquisitions['acquired_quarter'].dt.year.astype(str) + '-Q' + crunchbase_acquisitions['acquired_quarter'].dt.quarter.astype(str)
+crunchbase_acquisitions.groupby('quarter').size().to_frame('size').reset_index().sort_values('size', ascending=False)
