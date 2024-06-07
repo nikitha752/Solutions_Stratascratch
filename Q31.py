232c624dada490817837cb8b5e99ab0abe64361c
Nikitha <75156918+nikitha752@users.noreply.github.com>
2024-06-08 03:15:00 +0530

    Update Q31.py

--- Q31.py
+++ Q31.py
@@ -1,1 +1,5 @@
+import pandas as pd
 
+df = pd.merge(excel_sql_transaction_data, excel_sql_inventory_data, left_on='product_id', right_on='product_id', how='left')
+df = df.groupby(['product_id', 'product_name']).size().to_frame('size').reset_index().sort_values('product_id')
+df = df[['product_name', 'size']]
